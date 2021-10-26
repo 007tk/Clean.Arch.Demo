4530341db806dbf2b5135a2208d09f74fb42ced6
Tikane <tikane.kgoatla1@gmail.com>
2021-10-26 23:43:01 +0200

    implementation of service and repo of get courses methods.

--- CleanArch/CleanArch.Apllication/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Apllication/Interfaces/ICourseService.cs
@@ -7,7 +7,7 @@ namespace CleanArch.Application.Interfaces
 {
     public interface ICourseService
     {
-        IEnumerable<CourseViewModel> GetCousres();
+        CourseViewModel GetCousres();
 
 
     }
---
+++ CleanArch/CleanArch.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Data.Context;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Data.Repository
+{
+    class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCousres()
+        {
+            return _context.Courses;
+        }
+    }
+}
